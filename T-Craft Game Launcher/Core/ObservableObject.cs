649f3942d676a9a7d9df20a344379d8b945d5422
Tim Dürr <tim@duerrs.ch>
2023-02-04 00:00:34 +0100

    Add project files.

---
+++ T-Craft Game Launcher/Core/ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace T_Craft_Game_Launcher.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ T-Craft Game Launcher/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using T_Craft_Game_Launcher.Core;
+
+namespace T_Craft_Game_Launcher.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand SettingsViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public SettingsViewModel SettingsVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get
+            {
+                return _currentView;
+            }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            SettingsVM = new SettingsViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+            SettingsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SettingsVM;
+            });
+        }
+    }
+}
