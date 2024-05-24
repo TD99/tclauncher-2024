c3126854d03b97280f43b35da193bc582f0388ca
Tim Dürr <tim@duerrs.ch>
2024-05-24 16:52:45 +0200

    feat(CORS): Added CORS handling to SimpleWebServer

--- TCLauncher/Models/SimpleHTTPServer.cs
+++ TCLauncher/Models/SimpleHTTPServer.cs
@@ -13,16 +13,20 @@ namespace TCLauncher.Models
     {
         private readonly HttpListener _listener = new HttpListener();
         private readonly Func<HttpListenerRequest, string> _responderMethod;
+        private readonly bool _useDefaultCorsConfiguration;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SimpleHttpServer"/> class.
         /// </summary>
         /// <param name="prefixes">The prefixes to add to the listener.</param>
         /// <param name="method">The method to respond to HTTP requests.</param>
+        /// <param name="useDefaultCorsConfiguration">Whether to use the default CORS configuration.</param>
         /// <exception cref="ArgumentException"></exception>
         /// <exception cref="NotSupportedException"></exception>
-        public SimpleHttpServer(IReadOnlyCollection<string> prefixes, Func<HttpListenerRequest, string> method)
+        public SimpleHttpServer(IReadOnlyCollection<string> prefixes, Func<HttpListenerRequest, string> method, bool useDefaultCorsConfiguration = false)
         {
+            _useDefaultCorsConfiguration = useDefaultCorsConfiguration;
+
             if (!HttpListener.IsSupported)
                 throw new NotSupportedException("Needs Windows XP SP2, Server 2003 or later.");
 
@@ -43,6 +47,15 @@ namespace TCLauncher.Models
         /// <param name="prefixes">The prefixes to add to the listener.</param>
         public SimpleHttpServer(Func<HttpListenerRequest, string> method, params string[] prefixes)
             : this(prefixes, method) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleHttpServer"/> class.
+        /// </summary>
+        /// <param name="method">The method to respond to HTTP requests.</param>
+        /// <param name="useCors">Whether to use CORS.</param>
+        /// <param name="prefixes">The prefixes to add to the listener.</param>
+        public SimpleHttpServer(Func<HttpListenerRequest, string> method, bool useCors, params string[] prefixes)
+            : this(prefixes, method, useCors) { }
 
         /// <summary>
         /// Starts the HTTP server.
@@ -66,6 +79,12 @@ namespace TCLauncher.Models
                                 var buf = System.Text.Encoding.UTF8.GetBytes(rstr);
                                 ctx.Response.ContentLength64 = buf.Length;
                                 ctx.Response.ContentType = "application/json";
+                                if (_useDefaultCorsConfiguration)
+                                {
+                                    ctx.Response.AddHeader("Access-Control-Allow-Origin", "*");
+                                    ctx.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                                    ctx.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+                                }
                                 ctx.Response.OutputStream.Write(buf, 0, buf.Length);
                             }
                             catch
