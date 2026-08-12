9071730142ca0f2feed6adb01606a6089eed584a
Tim Dürr <tim@duerrs.ch>
2026-08-12 23:41:04 +0200

    Style context menus consistently across the application

---
+++ TCLauncher.Controls.Gallery/ContextMenuPage.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows.Controls;
+
+namespace TCLauncher.Controls.Gallery
+{
+    [Story("Context Menu")]
+    public partial class ContextMenuPage : UserControl
+    {
+        public ContextMenuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
